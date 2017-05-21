960bb4ff64e3fe95cb1ee11ae9a7ee88ea93fed3
jen2 <sipila.jen@gmail.com>
2017-05-21 09:11:34 -0400

    initial commit

--- NotificationsUI/AppDelegate.swift
+++ NotificationsUI/AppDelegate.swift
@@ -2,45 +2,80 @@
 //  AppDelegate.swift
 //  NotificationsUI
 //
-//  Created by Pranjal Satija on 9/12/16.
-//  Copyright © 2016 Pranjal Satija. All rights reserved.
+//  Created by Jennifer Sipila on 5/12/17.
+//  Copyright © 2017 Jennifer Sipila. All rights reserved.
 //
 
 import UIKit
+import UserNotifications
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+@UIApplicationMain class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+        [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { (accepted, error) in
+            if !accepted {
+                print("Notification access denied")
+            }
+        }
         return true
     }
-
+    
+    func scheduleNotification(at date: Date) {
+        let calendar = Calendar(identifier: .gregorian)
+        let components = calendar.dateComponents(in: .current, from: date)
+        let newComponents = DateComponents(calendar: calendar, timeZone: .current, month: components.month, day: components.day, hour: components.hour, minute: components.minute)
+    
+        let trigger = UNCalendarNotificationTrigger(dateMatching: newComponents, repeats: false)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Test Local Notification"
+        content.body = "Just a test to see if notifications is up and running"
+        content.sound = UNNotificationSound.default()
+        
+        if let path = Bundle.main.path(forResource: "Working", ofType: "jpg") {
+            let url = URL(fileURLWithPath: path)
+            
+            do {
+                let attachment = try UNNotificationAttachment(identifier: "Working", url: url, options: nil)
+                content.attachments = [attachment]
+            } catch {
+                print("The attachment was not loaded.")
+            }
+        }
+        
+        let request = UNNotificationRequest(identifier: "textNotification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().add(request) {(error) in
+            if let error = error {
+                print("Uh oh! We had an error: \(error)")
+            }
+        }
+    }
+    
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
-
--- NotificationsUI/ViewController.swift
+++ NotificationsUI/ViewController.swift
@@ -2,24 +2,19 @@
 //  ViewController.swift
 //  NotificationsUI
 //
-//  Created by Pranjal Satija on 9/12/16.
-//  Copyright © 2016 Pranjal Satija. All rights reserved.
+//  Created by Jennifer Sipila on 5/12/17.
+//  Copyright © 2017 Jennifer Sipila. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func datePickerDidSelectNewDate(_ sender: UIDatePicker) {
+        
+        let selectedDate = sender.date
+        let delegate = UIApplication.shared.delegate as? AppDelegate
+        
+        delegate?.scheduleNotification(at: selectedDate)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
